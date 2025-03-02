b3b75401ed3975abe199bdaf6cf2166dde54e47b
Luke Moore <luke.moore@gmail.com>
2025-03-02 14:36:14 -0500

    Supporting Python 3.6 and setuptools<61.0.

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages, Extension
 
 
+# Note that for Python 3.6, which only supports setuptools<=59.6.0, we need
+# to specify the name and version, pyproject.toml is only supported in >=61.0.
 setup(
+    name="resumablesha256",
+    version="1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=False,
